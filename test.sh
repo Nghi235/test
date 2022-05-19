a8c1a95a25085b63dc79cbc5f9ad5a959645d016
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 17:05:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-clear
-read -p "Vui Lòng Nhập Key: " key
 
-if [[ $key == nguyenvannghi ]]; then
-echo "Xác Thực Thành Công" 
-sleep 5
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/test/main/test1.sh)
-elif [[ $key == dangvanhoang ]]; then
-echo "Xác Thực Thành Công" 
-sleep 5
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/test/main/test1.sh)
-else echo "Key Không Chính Xác !"
-exit
-fi
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/key.sh)
 
