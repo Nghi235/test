a48f4a03ad8fd46f1d5d4cbda2d2d0a5a5ceffb9
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 15:57:25 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,9 @@
 read -p "Vui Lòng Nhập Domain: " domain
 
 if [[ $domain == nghi ]]; then echo "speed4g.xyz"
-else echo "Website Không Được Cấp Phép"
+elif echo "Website Không Được Cấp Phép"
 fi
-
 if [[ $domain == hoang ]]; then echo "vpndata.xyz"
-else echo "Website Không Được Cấp Phép !"
+elif echo "Website Không Được Cấp Phép !"
 fi
 
