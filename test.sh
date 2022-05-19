41195c2fff246056d08d00bad03f05c4ee24ea4b
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 15:17:21 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-confirm "Vui Lòng Nhập Domain : $domain"
-
-if [[$domain == nghi]]; then echo "vippro"
-else echo "Không Phù Hợp"
-
+read -s -p "Vui Lòng Nhập Domain: " PASSWD
+printf "%b"
