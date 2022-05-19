fac37a59a66daf04fcf58e9a3c2034ff50b6936d
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 15:50:31 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 read -p "Vui Lòng Nhập Domain: " domain
 
-if [[ $domain == nghi ]]; then echo "vippro"
-else echo "Kj"
+if [[ $domain == nghi ]]; then echo "speed4g.xyz"
+else echo "Website Không Được Cấp Phép"
 fi
+
+if [[ $domain == hoang ]]; then echo "vpndata.xyz"
+else echo "Website Không Được Cấp Phép !"
+fi
+
