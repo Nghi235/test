a6dd7137ee08f03e885ca38a0f947f5909fd1745
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 15:11:32 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+checkdomain() {
+confirm "Vui Lòng Nhập Domain : $domain"
 
-Vui Lòng Nhập Domain : $domain
-
-if [[$domain = nghi]]; then echo "vippro"
+if [[$domain == nghi]]; then echo "vippro"
 else echo "Không Phù Hợp"
+}
