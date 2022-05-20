01d8b9de4bd8eb9b56a06cd70c080f990789453c
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-20 22:31:59 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-read -p "Vui Lòng Nhập Domain: " domain
-echo "di chuyển"
-cd /www/wwwroot/${domain}/
+git clone https://github.com/Nghi235/xflash.git ./
+unzip -u XFLASH1.zip
+unzip XFLASH2.zip
+rm -rf XFASH1.zip
+rm -rf XFLASH2.zip
+clear
+composer update
+php artisan package:discover --ansi
+composer dump-autoload --ignore-platform-reqs
+composer dump-autoload
+sh init.sh
