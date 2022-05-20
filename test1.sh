97e2db7898592ca3ebeb86def46654936523bf7f
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-20 20:57:53 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 read -p "Vui Lòng Nhập Domain: " domain
+check=$(find /www/wwwroot -type d -name $domain)
+if [[ $ == /www/wwwroot/$domain]]; then 
 
-if [[ {find /www/wwwroot -type d -name $domain} == {/www/wwwroot/${domain}}]]; then 
-
-cd /www/wwwroot/${domain}
+cd /www/wwwroot/$domain
 
 else
 exit
