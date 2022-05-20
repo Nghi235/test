d380512b76c82bf2d2012d8d85276b55d6a82f61
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-20 21:22:22 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 read -p "Vui Lòng Nhập Domain: " domain
 check=$(find /www/wwwroot -type d -name $domain)
+xyz=$(/www/wwwroot/${domain})
 if [[ $check == /www/wwwroot/${domain} ]]; then 
-cd /www/wwwroot/${domain}
+cd $xyz
 else
 echo "VIP"
 fi 
