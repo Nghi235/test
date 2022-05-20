06a2625b97c4d70fd674ef7fa4393f766e64b098
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-20 20:19:52 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-    echo -e "
-    
-————————————————————————————————
-    1. SPEED4G.XYZ
-————————————————————————————————
-    2. VPNDATA.XYZ
-————————————————————————————————
-   
- "
- 
-    echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
-
-    case "${num}" in
-        1) echo " ADMIN Nguyễn Nghị"
-        ;;
-        2) echo " ADMIN Nguyễn Nghị"
-        ;;
-        *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2"
-        ;;
-    esac
-
+read -p "Vui Lòng Nhập Domain: " domain
+cd /www/wwwroot/$domain
