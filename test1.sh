2ceb7b0a278ff2ef3d45dbd4e130ffaec96c5f42
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-21 21:26:30 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Nghi235/xflash.git ./
-unzip -u XFLASH1.zip
-unzip XFLASH2.zip
-rm -rf XFASH1.zip
-rm -rf XFLASH2.zip
-clear
-composer update
-php artisan package:discover --ansi
-composer dump-autoload --ignore-platform-reqs
-composer dump-autoload
-sh init.sh
+read -p "Nhập Link" link
+cd $link
