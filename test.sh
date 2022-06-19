55d16c0a3f3ceea4c9b31b52e6ba77bbad918bea
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-19 14:51:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 check=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-if [[ $check == 100 ]]; then
 check1=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-if [[ $check1 == 100 ]]; then
 check2=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-if [[ $check2 == 100 ]]; then
+if [[ $check == 100 ]];[[ $check1 == 100 ]];[[ $check2 == 100 ]]; then
+
 curl -X PATCH "https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/settings/security_level" \
      -H "X-Auth-Email: nghicoi2003@gmail.com" \
      -H "X-Auth-Key: 50bc64db5f95bc6e98ee9b1275b1440bb00d4" \
