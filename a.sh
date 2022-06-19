7e09aaac593da7068da43c98c75efc9707b5e191
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-19 13:59:43 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,3 @@
-curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/firewall/rules" \
+curl -X GET "https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/firewall/rules" \
 -H "X-Auth-Email: nghicoi2003@gmail.com" \
 -H "X-Auth-Key: 50bc64db5f95bc6e98ee9b1275b1440bb00d4"
