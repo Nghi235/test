bedb2f7b4dd18f670954c445ed5396b15f94b2d5
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-19 13:51:48 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-curl -X DELETE \
-"https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/firewall/rules/09714c593bd743a895d2967d81c335cc" \
+curl -X GET \
+"https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/firewall/rules" \
 -H "X-Auth-Email: nghicoi2003@gmail.com" \
 -H "X-Auth-Key: 50bc64db5f95bc6e98ee9b1275b1440bb00d4"
