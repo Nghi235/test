fced00151786d8b11f0400f9deaac483560eee4f
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-19 13:07:42 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-currload=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-echo "$currload"
-  
-
+check=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+if [[ $check == 100 ]]; then
+echo "Đang Bị DDoS !" 
+else 
+echo "Không Bị DDoS !"
+exit
+fi
