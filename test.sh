ff4f9b240c2acfcfc95b16c2974748afa9544945
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-19 13:18:33 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 check=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 if [[ $check == 100 ]]; then
-echo "Đang Bị DDoS !" 
+curl -X PATCH "https://api.cloudflare.com/client/v4/zones/ba1f3c31034ed52f8c7e266c7619d0a0/settings/security_level" \
+     -H "X-Auth-Email: nghicoi2003@gmail.com" \
+     -H "X-Auth-Key: 50bc64db5f95bc6e98ee9b1275b1440bb00d4" \
+     -H "Content-Type: application/json" \
+     --data '{"value":"under_attack"}'
 else 
 echo "Không Bị DDoS !"
 exit
